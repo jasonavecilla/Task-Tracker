7ff50c5beed87fcc1cda350179a1aa20478592a0
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2024-01-17 10:32:27 -0500

    finished setup for the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-500">
+          V47-Tier2-Team-19
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
