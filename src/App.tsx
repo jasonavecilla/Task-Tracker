63a73fd83c12411f0afa9882fda9390e0b5bcd36
bscaer <bscaer@gmail.com>
2024-01-31 16:09:33 -0500

    Implemented Save button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import TableHeader from "./components/TableHeader";
+import Button from '@mui/material/Button';
+
+const Save = () => alert('clicked');
 
 function App() {
   return (
     <>
+      <div className='align-left'>
+        <Button variant="contained" onClick={Save}>Save</Button>
+      </div>
       <div>
         <h1 className="text-3xl font-bold underline text-red-500">
           V47-Tier2-Team-19
